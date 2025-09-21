2cd8962eec0e0836a19cf1acc5a7f26ce5f2e6fe
chandanpalariya <cpalariya67@gmail.com>
2025-09-21 09:10:58 +0530

    add frontent

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
   "/uploads",
   express.static(path.join(_dirname, "uploads"), {
     setHeaders: (res, _path) => {
-      res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+      res.set("Access-Control-Allow-Origin", "https://resumebuilder-3-b0v6.onrender.com/");
     },
   })
 );
